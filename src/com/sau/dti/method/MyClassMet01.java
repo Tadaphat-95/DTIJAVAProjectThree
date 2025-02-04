d601b679aff2e31b1f1ab11e1c01328bec85a293
Tadaphat-95 <s6752410028@gmail.com>
2025-02-04 15:01:44 +0700

    v.2 Overloading method

--- src/com/sau/dti/method/MyClassMet01.java
+++ src/com/sau/dti/method/MyClassMet01.java
@@ -1,5 +1,24 @@
 package com.sau.dti.method;
 
 public class MyClassMet01 {
-    
+    //method = function = การทำงานหนึ่งๆ ที่จะไม่ทำหากไม่ใช้เรียกใช้ (call method)
+    public static void showHi(){
+        System.out.println("Hi...");
+    }
+    public static void showHello(){
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        showHi();
+        showHi();
+        showHello();
+    }
+    public static void showHey(){
+        System.out.println("Hey");
+    }
+
+
+
+
 }
